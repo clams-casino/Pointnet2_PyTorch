4fd1efe95aae9598ad32e01283d97b891e6a4fbf
mike <mike.zhang.619@gmail.com>
2021-06-26 01:00:04 +0200

    make group_points templated

--- pointnet2_ops_lib/pointnet2_ops/_ext-src/src/group_points_gpu.cu
+++ pointnet2_ops_lib/pointnet2_ops/_ext-src/src/group_points_gpu.cu
@@ -5,11 +5,12 @@
 
 // input: points(b, c, n) idx(b, npoints, nsample)
 // output: out(b, c, npoints, nsample)
+template <typename scalar_t>
 __global__ void group_points_kernel(int b, int c, int n, int npoints,
                                     int nsample,
-                                    const float *__restrict__ points,
+                                    const scalar_t *__restrict__ points,
                                     const int *__restrict__ idx,
-                                    float *__restrict__ out) {
+                                    scalar_t *__restrict__ out) {
   int batch_index = blockIdx.x;
   points += batch_index * n * c;
   idx += batch_index * npoints * nsample;
@@ -28,23 +29,32 @@ __global__ void group_points_kernel(int b, int c, int n, int npoints,
 }
 
 void group_points_kernel_wrapper(int b, int c, int n, int npoints, int nsample,
-                                 const float *points, const int *idx,
-                                 float *out) {
+                                 at::Tensor points, const int *idx,
+                                 at::Tensor &out) {
   cudaStream_t stream = at::cuda::getCurrentCUDAStream();
 
-  group_points_kernel<<<b, opt_block_config(npoints, c), 0, stream>>>(
-      b, c, n, npoints, nsample, points, idx, out);
+  // group_points_kernel<<<b, opt_block_config(npoints, c), 0, stream>>>( #TODO remove this
+  //     b, c, n, npoints, nsample, points, idx, out);
+
+  AT_DISPATCH_FLOATING_TYPES_AND_HALF(points.type(), "group_points", ([&] {
+    group_points_kernel<scalar_t><<<b, opt_block_config(npoints, c), 0, stream>>>(
+        b, c, n, npoints, nsample,
+        points.data_ptr<scalar_t>(),
+        idx,
+        out.data_ptr<scalar_t>());
+  }));
 
   CUDA_CHECK_ERRORS();
 }
 
 // input: grad_out(b, c, npoints, nsample), idx(b, npoints, nsample)
 // output: grad_points(b, c, n)
+template <typename scalar_t>
 __global__ void group_points_grad_kernel(int b, int c, int n, int npoints,
                                          int nsample,
-                                         const float *__restrict__ grad_out,
+                                         const scalar_t *__restrict__ grad_out,
                                          const int *__restrict__ idx,
-                                         float *__restrict__ grad_points) {
+                                         scalar_t *__restrict__ grad_points) {
   int batch_index = blockIdx.x;
   grad_out += batch_index * npoints * nsample * c;
   idx += batch_index * npoints * nsample;
@@ -64,12 +74,21 @@ __global__ void group_points_grad_kernel(int b, int c, int n, int npoints,
 }
 
 void group_points_grad_kernel_wrapper(int b, int c, int n, int npoints,
-                                      int nsample, const float *grad_out,
-                                      const int *idx, float *grad_points) {
+                                      int nsample, at::Tensor grad_out,
+                                      const int *idx, at::Tensor &grad_points) {
   cudaStream_t stream = at::cuda::getCurrentCUDAStream();
 
-  group_points_grad_kernel<<<b, opt_block_config(npoints, c), 0, stream>>>(
-      b, c, n, npoints, nsample, grad_out, idx, grad_points);
+  // group_points_grad_kernel<<<b, opt_block_config(npoints, c), 0, stream>>>(
+  //     b, c, n, npoints, nsample, grad_out, idx, grad_points);
+
+
+  AT_DISPATCH_FLOATING_TYPES_AND_HALF(grad_out.type(), "group_points_grad", ([&] {
+    group_points_grad_kernel<scalar_t><<<b, opt_block_config(npoints, c), 0, stream>>>(
+        b, c, n, npoints, nsample,
+        grad_out.data_ptr<scalar_t>(),
+        idx,
+        grad_points.data_ptr<scalar_t>());
+  }));
 
   CUDA_CHECK_ERRORS();
 }
